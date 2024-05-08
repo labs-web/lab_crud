5cad3984dd1cebdaa066bad81fc37f5889c6892d
Hamid Achaou <hamidAchaou379@gmail.com>
2024-05-08 16:55:27 +0100

    import ckeditor

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import 'bootstrap';
+
+// Import CKEditor
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+document.addEventListener('DOMContentLoaded', function() {
+    ClassicEditor
+        .create(document.querySelector('#editor'))
+        .then(editor => {
+            window.editor = editor;
+        })
+        .catch(error => {
+            console.error('There was a problem initializing the editor.', error);
+        });
+});
