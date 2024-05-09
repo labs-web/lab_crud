ac6a92f0b391f657c3997a01d4a1aed64c5ca953
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2024-05-09 11:21:22 +0100

    update

--- app/lang/fr/GestionProjets/projet/message.php
+++ app/lang/fr/GestionProjets/projet/message.php
@@ -6,7 +6,4 @@ return [
     'detail' => 'Detail de projet',
     'createProjectException' => 'Projet est déjà existant',
     'unexpected_error' => 'Une erreur inattendue s\'est produite. Veuillez réessayer.',
-    
-   
-
 ];
