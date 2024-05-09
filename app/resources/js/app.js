a43b6a944331957cf98d4c8b6f59a10538571f92
Hamid <hamidachaou379@gmail.com>
2024-05-09 01:50:12 +0100

    fixe  ck editor

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -2,13 +2,12 @@ import 'bootstrap';
 
 // Import CKEditor
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-document.addEventListener('DOMContentLoaded', function() {
-    ClassicEditor
-        .create(document.querySelector('#editor'))
-        .then(editor => {
-            window.editor = editor;
-        })
-        .catch(error => {
-            console.error('There was a problem initializing the editor.', error);
-        });
-});
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ) )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( error => {
+		console.error( 'There was a problem initializing the editor.', error );
+	} );
