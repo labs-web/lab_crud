f9f196311542525817e8af483b39d4a34e68e2d2
= <=>
2024-05-10 11:01:18 +0100

    save

--- app/routes/GestionProjets/TaskRoute.php
+++ app/routes/GestionProjets/TaskRoute.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\GestionTasks\TaskController;
 // TODO : Commentaire : routage par Model
 
 Route::middleware('auth')->group(function () {
-    Route::prefix('/')->group(function () {
+    Route::prefix('/tasks')->group(function () {
         Route::resource('tasks', TaskController::class);
         Route::get('export', [TaskController::class, 'export'])->name('tasks.export');
         Route::post('import', [TaskController::class, 'import'])->name('tasks.import');
