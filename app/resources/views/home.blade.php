01d819c3ddfca55df50f9ec4f9d7b404fa4c126d
Hamid <hamidachaou379@gmail.com>
2024-05-10 21:59:56 +0100

    edit home page

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -1,36 +1,12 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
-
-
 @extends('layouts.app')
+@section('title', __('app.home'))
 
 @section('content')
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Statistiques</h1>
+                    <h1>{{ __('Statistiques') }}</h1>
                 </div>
             </div>
         </div>
@@ -43,12 +19,12 @@
                     <div class="small-box bg-info">
                         <div class="inner">
                             <h3>23</h3>
-                            <p>Projets</p>
+                            <p>{{ __('Projets') }}</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Plus d'informations <i
+                        <a href="#" class="small-box-footer">{{ __('Plus d\'informations') }} <i
                                 class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
@@ -58,12 +34,12 @@
                     <div class="small-box bg-success">
                         <div class="inner">
                             <h3>13</h3>
-                            <p>Administrateurs</p>
+                            <p>{{ __('Administrateurs') }}</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Plus d'informations <i
+                        <a href="#" class="small-box-footer">{{ __('Plus d\'informations') }} <i
                                 class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
@@ -73,12 +49,12 @@
                     <div class="small-box bg-warning">
                         <div class="inner">
                             <h3>6</h3>
-                            <p>Congé actuel</p>
+                            <p>{{ __('Congé actuel') }}</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-pie-graph"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Plus d'informations <i
+                        <a href="#" class="small-box-footer">{{ __('Plus d\'informations') }} <i
                                 class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
@@ -88,13 +64,13 @@
 
                     <div class="small-box bg-danger">
                         <div class="inner">
-                            <h3>70 Jours</h3>
-                            <p>Absences</p>
+                            <h3>70 {{ __('Jours') }}</h3>
+                            <p>{{ __('Absences') }}</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Plus d'informations <i
+                        <a href="#" class="small-box-footer">{{ __('Plus d\'informations') }} <i
                                 class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home*') || Request::is('/') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>
             {{ __('app.home') }}
