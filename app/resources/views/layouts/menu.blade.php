120cc9d83d07c595fe16de65f92a2b951757379c
Hamid Achaou <hamidAchaou379@gmail.com>
2024-05-10 12:29:02 +0100

    add lange tache

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,20 +1,30 @@
-<!-- TODO : Organisation de menu package -->
 <!-- need to remove -->
 <li class="nav-item">
     <a href="{{ route('home') }}" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>
-            Accueil
+            {{ __('app.home') }}
         </p>
     </a>
 </li>
+{{-- project --}}
 <li class="nav-item">
     <a href="{{ route('projets.index') }}"
         class="nav-link {{ Request::is('projets*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-table"></i>
         <p>
-            Projets
+            {{ __('GestionProjets/projet.plural') }}
         </p>
     </a>
 </li>
-{{-- @endcan --}}
+{{-- task --}}
+<li class="nav-item">
+    <a href="{{ route('tasks.index') }}"
+        class="nav-link {{ Request::is('tasks*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-table"></i>
+        <p>
+            {{ __('GestionProjets/tache.plural') }}
+        </p>
+    </a>
+</li>
+
